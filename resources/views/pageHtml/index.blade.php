e21c5956651c208b66ffcc3c221d9d264df999c1
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-08-05 08:52:35 +0100

    Update index.blade.php

--- resources/views/pageHtml/index.blade.php
+++ resources/views/pageHtml/index.blade.php
@@ -770,7 +770,7 @@
     <section id="gallery">
         <div class="empty-space col-md-b100 col-xs-b70"></div>
         <div class="tm-section-heading text-center">
-            <h2>View our Gallery</h2>
+            <h2>Voir notre galerie</h2>
             <div class="tm-section-seperator"><span></span></div>
             <div class="empty-space col-md-b60 col-xs-b40"></div>
         </div>
